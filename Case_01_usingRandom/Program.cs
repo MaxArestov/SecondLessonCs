a3a9fb4a9df66d975896b8fbde8a25ff69df1503
maxim arestov <aresmax1993@gmail.com>
2022-10-16 17:18:25 +0300

    Added random to random for random

---
+++ Case_01_usingRandom/Program.cs
@@ -0,0 +1,21 @@
+﻿int randomNumber = GetRandomNumber(1, 999999);
+Console.Write("Сгенерировалось число ");
+Console.WriteLine(randomNumber);
+int max = randomNumber % 10;
+randomNumber = randomNumber / 10;
+while(randomNumber > 0)
+{
+    int lastNumber = randomNumber % 10;
+    if(lastNumber > max)
+    {
+        max = lastNumber;
+    }
+    randomNumber = randomNumber / 10;    
+}
+Console.WriteLine($"максимальное цифра в сгенерированном - {max}");
+int GetRandomNumber(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
